4724e7a0320a489b3f8057ee70a34f47dc13a609
Nelson Wells <nelsonwellswku@gmail.com>
2016-01-16 00:54:31 -0600

    Add failing unit tests for dropping a piece into the board

--- src/Application.Tests/GameBoardTests.cs
+++ src/Application.Tests/GameBoardTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
 using Octogami.ConnectFour.Application.Game;
@@ -7,21 +8,78 @@ namespace Octogami.ConnectFour.Application.Tests
 {
 	public class GameBoardTests
 	{
+		private GameBoard _gameBoard;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_gameBoard = new GameBoard();
+		}
+
 		[Test]
 		public void Construct_an_empty_board()
 		{
-			// Arrange
-
-			// Act
-			var gameBoard = new GameBoard();
-
 			// Assert
-			gameBoard.Board.Count.Should().Be(6, "becase the board should have 6 rows");
-			foreach(var row in gameBoard.Board)
+			_gameBoard.Board.Count.Should().Be(6, "becase the board should have 6 rows");
+			foreach(var row in _gameBoard.Board)
 			{
 				row.Count.Should().Be(7, "because each row should have 7 columns");
 			}
-			gameBoard.Board.SelectMany(x => x).Distinct().Single().Should().Be(BoardPiece.Empty, "because the board should start out empty");
+			_gameBoard.Board.SelectMany(x => x).Distinct().Single().Should().Be(BoardPiece.Empty, "because the board should start out empty");
+		}
+
+		[Test]
+		public void Can_not_drop_an_empty_piece()
+		{
+			// Act - Assert
+			Action act = () => _gameBoard.TryDropPiece(BoardPiece.Empty, GameBoardColumn.Zero);
+			act.ShouldThrowExactly<InvalidOperationException>().WithMessage("Can not drop an empty piece into the game board");
+		}
+
+		[TestCase(0)]
+		[TestCase(1)]
+		[TestCase(2)]
+		[TestCase(3)]
+		[TestCase(4)]
+		[TestCase(5)]
+		[TestCase(6)]
+		public void Can_drop_pieces_in_columns(int columnNumber)
+		{
+			var column =
+				columnNumber == 1 ? GameBoardColumn.One :
+				columnNumber == 2 ? GameBoardColumn.Two :
+				columnNumber == 3 ? GameBoardColumn.Three :
+				columnNumber == 4 ? GameBoardColumn.Four :
+				columnNumber == 5 ? GameBoardColumn.Five :
+				columnNumber == 6 ? GameBoardColumn.Six : 
+					GameBoardColumn.Zero;
+
+			for(var i = 5; i >= 0; i--)
+			{
+				var pieceToDrop = i % 2 == 0 ? BoardPiece.PlayerOne : BoardPiece.PlayerTwo;
+
+				var result = _gameBoard.TryDropPiece(pieceToDrop, column);
+
+				result.Should().BeTrue();
+				_gameBoard.Board[columnNumber][i].Should().Be(pieceToDrop);
+			}
+		}
+
+		[Test]
+		public void Can_not_drop_more_pieces_in_a_column_than_the_board_can_accomodate()
+		{
+			// Arrange
+			for(int i = 0; i <= 6; i++)
+			{
+				_gameBoard.TryDropPiece(BoardPiece.PlayerOne, GameBoardColumn.Zero);
+			}
+
+			// Act
+			var result = _gameBoard.TryDropPiece(BoardPiece.PlayerTwo, GameBoardColumn.Zero);
+
+			// Assert
+			result.Should().BeFalse();
+			_gameBoard.Board[0].Select(x => x).Distinct().Single().Should().Be(BoardPiece.PlayerOne);
 
 		}
 	}
--- src/Application/Game/GameBoard.cs
+++ src/Application/Game/GameBoard.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Octogami.ConnectFour.Application.Game
@@ -28,5 +29,28 @@ namespace Octogami.ConnectFour.Application.Game
 		}
 
 		public IReadOnlyList<IReadOnlyList<BoardPiece>> Board => _board;
+
+		public bool TryDropPiece(BoardPiece piece, GameBoardColumn column)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public struct GameBoardColumn
+	{
+		private GameBoardColumn(int column)
+		{
+			Column = column;
+		}
+
+		public int Column { get; }
+
+		public static GameBoardColumn Zero => new GameBoardColumn(0);
+		public static GameBoardColumn One => new GameBoardColumn(1);
+		public static GameBoardColumn Two => new GameBoardColumn(2);
+		public static GameBoardColumn Three => new GameBoardColumn(3);
+		public static GameBoardColumn Four => new GameBoardColumn(4);
+		public static GameBoardColumn Five => new GameBoardColumn(5);
+		public static GameBoardColumn Six => new GameBoardColumn(6);
 	}
 }
