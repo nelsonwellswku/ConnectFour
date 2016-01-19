c4f97d0629ff1298cf7d2fae2c76a054dbb2672b
Nelson Wells <nelsonwellswku@gmail.com>
2016-01-18 23:11:35 -0600

    GameActor accepts a MakeMove message and updates the game board

--- src/Application/Actor/GameActor.cs
+++ src/Application/Actor/GameActor.cs
@@ -22,12 +22,9 @@ namespace Octogami.ConnectFour.Application.Actor
 		{
 			Receive<JoinGameById>(msg =>
 			{
-				// Assign, not generate, the game id only when a new game is created 
+				// Assign, not generate, the game id only when a new game is created
 				// because the game id is assigned by the game manager, not the game itself
-				if(_gameId == default(Guid))
-				{
-					_gameId = msg.GameId;
-				}
+				_gameId = msg.GameId;
 
 				_playerOne = msg.Username;
 				var playerSlot = "PlayerOne";
@@ -54,6 +51,44 @@ namespace Octogami.ConnectFour.Application.Actor
 		public void FullGame()
 		{
 			Receive<JoinGameById>(msg => Sender.Tell(new JoinGameRejectionMessage(msg.Username, msg.GameId, "Game full")));
+
+			Receive<MakeMove>(msg =>
+			{
+				var isPlayerOne = msg.Username == _playerOne;
+
+				var column = GameBoardColumn.Zero;
+				switch(msg.Column)
+				{
+					case 0: column = GameBoardColumn.Zero;	break;
+					case 1: column = GameBoardColumn.One;	break;
+					case 2: column = GameBoardColumn.Two;	break;
+					case 3: column = GameBoardColumn.Three;	break;
+					case 4: column = GameBoardColumn.Four;	break;
+					case 5: column = GameBoardColumn.Five;	break;
+
+					// TODO: Send a failure message back to the sender
+					default: break;
+				}
+
+				_gameBoard.DropPiece(isPlayerOne ? BoardPiece.PlayerOne : BoardPiece.PlayerTwo, column);
+
+				var playerOneWins = _gameBoard.IsGameOver(BoardPiece.PlayerOne);
+				var playerTwoWins = _gameBoard.IsGameOver(BoardPiece.PlayerTwo);
+
+				string winner = null;
+				if(playerOneWins)
+				{
+					winner = _playerOne;
+				}
+				else if(playerTwoWins)
+				{
+					winner = _playerTwo;
+				}
+
+				// TODO: Need to check if the game is a draw and include that in the message. This should be a method in the GameBoard class.
+
+				Sender.Tell(new GameStatusMessage(_gameBoard.Board, playerOneWins || playerTwoWins, winner));
+			});
 		}
 	}
 }
---
+++ src/Application/Actor/PlayGameMessages.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Octogami.ConnectFour.Application.Game;
+
+namespace Octogami.ConnectFour.Application.Actor
+{
+	public class MakeMove
+	{
+		public MakeMove(string username, int column)
+		{
+			Username = username;
+			Column = column;
+		}
+		public string Username;
+		public int Column;
+	}
+
+	public class GameStatusMessage
+	{
+		public IReadOnlyList<IReadOnlyList<BoardPiece>> Board { get; set; }
+		public bool IsGameOver { get; set; }
+		public string Winner { get; set; }
+
+		public GameStatusMessage(IReadOnlyList<IReadOnlyList<BoardPiece>> board, bool isGameOver, string winner)
+		{
+			Board = board;
+			IsGameOver = isGameOver;
+			Winner = winner;
+		}
+	}
+}
